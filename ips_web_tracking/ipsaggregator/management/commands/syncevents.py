ca2232ba9cb812ee196fd3ff492c12acf5b691c5
Prince Mishra <indiandexter007@gmail.com>
2016-11-05 14:27:58 +0530

    more verbose

--- ips_web_tracking/ipsaggregator/management/commands/syncevents.py
+++ ips_web_tracking/ipsaggregator/management/commands/syncevents.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import traceback
 
 from django.core.management.base import BaseCommand, CommandError
 from bs4 import BeautifulSoup
@@ -19,34 +20,41 @@ class Command(BaseCommand):
             html_doc = r.content
             soup = BeautifulSoup(html_doc, 'html.parser')
             tables = soup.find_all('table')
-            events_table = tables[-1]
-            rows = events_table.find_all('tr')[2:]
+            try:
+                events_table = tables[-1]
+                rows = events_table.find_all('tr')[2:]
 
-            for row in rows:
-                cols = row.find_all('td')
-                event_time = datetime.strptime(cols[0].text.strip(), '%m/%d/%Y %I:%M:%S %p')
-                event_type = cols[3].text.strip()
-                row_obj = {
-                    'shipment_id' : s.id,
-                    'event_time' : event_time,
-                    'country' : cols[1].text.strip(),
-                    'location' : cols[2].text.strip(),
-                    'event_type' : event_type,
-                    'mail_category' : cols[4].text.strip(),
-                    'next_office' : cols[5].text.strip(),
-                    'extra_info_col1' : cols[6].text.strip(),
-                    'extra_info_col2' : cols[7].text.strip(),
-                }
-                existing = ShipmentEvent.objects.filter(shipment_id = s.id, event_time = event_time)
-                if len(existing):
-                    pass#self.stdout.write(self.style.ERROR(row_obj))
-                else:
-                    ev = ShipmentEvent(**row_obj)
-                    ev.save()
-                    if 'Deliver item' in event_type:
-                        s.processed = True
-                        s.save()
-                    #self.stdout.write(self.style.SUCCESS(row_obj))
+                for row in rows:
+                    try:
+                        cols = row.find_all('td')
+                        event_time = datetime.strptime(cols[0].text.strip(), '%m/%d/%Y %I:%M:%S %p')
+                        event_type = cols[3].text.strip()
+                        row_obj = {
+                            'shipment_id' : s.id,
+                            'event_time' : event_time,
+                            'country' : cols[1].text.strip(),
+                            'location' : cols[2].text.strip(),
+                            'event_type' : event_type,
+                            'mail_category' : cols[4].text.strip(),
+                            'next_office' : cols[5].text.strip(),
+                            'extra_info_col1' : cols[6].text.strip(),
+                            'extra_info_col2' : cols[7].text.strip(),
+                        }
+                        existing = ShipmentEvent.objects.filter(shipment_id = s.id, event_time = event_time)
+                        if len(existing):
+                            pass#self.stdout.write(self.style.ERROR(row_obj))
+                        else:
+                            ev = ShipmentEvent(**row_obj)
+                            ev.save()
+                            if 'Deliver item' in event_type:
+                                s.processed = True
+                                s.save()
+                            #self.stdout.write(self.style.SUCCESS(row_obj))
+                    except Exception, fault:
+                        traceback.print_exc()
+            except Exception, fault:
+                print "Failed to sync shipment : %s" % s.identifier
+                traceback.print_exc()
 
 
 
